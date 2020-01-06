0261134adbbef8d3cf52e6cb5e9aaba985186063
Sagar D B (DropCodes) <sagardb50@gmail.com>
2020-01-06 21:07:02 +0530

    added PrimeNumber

---
+++ src/BasicsIO/Palindrome.java
@@ -0,0 +1,86 @@
+package BasicsIO;
+
+/*
+You have been given a String S. You need to find and print whether this string is a palindrome or not. If yes, print "YES" (without quotes), else print "NO" (without quotes).
+
+Input Format
+The first and only line of input contains the String S. The String shall consist of lowercase English alphabets only.
+
+Output Format
+Print the required answer on a single line.
+
+Constraints
+
+Note
+String S consists of lowercase English Alphabets only.
+
+SAMPLE INPUT
+aba
+SAMPLE OUTPUT
+YES
+ */
+
+import java.util.*;
+public class Palindrome {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        String str = s.nextLine().trim();
+
+        char[] char_1 = new char[str.length()];
+        for (int i = 0; i < str.length(); i++) {
+            char_1[i] = str.charAt(i);
+        }
+        Stack stack = new Stack(char_1.length);
+
+        for (char c : char_1) {
+            stack.push(c);
+        }
+        char[] char_2 = new char[char_1.length];
+
+        for (int i = 0; i < char_1.length; i++) {
+            char_2[i] = stack.pop();
+        }
+
+        String res_str = new String(char_2);
+        if (res_str.equals(str)) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+
+    }
+}
+
+//Stack Class
+class Stack {
+    private int top = -1;
+    private int size;
+    private char[] stack;
+
+    public Stack(int size) {
+        this.size = size;
+        top = -1;
+        stack = new char[size];
+    }
+
+    public void push(char ele) {
+        if (!isFull()) {
+            top++;
+            stack[top] = ele;
+        }
+    }
+
+    public char pop() {
+        return stack[top--];
+
+    }
+
+    public boolean isFull() {
+        if (top == stack.length - 1) {
+            return true;
+        }
+        return false;
+    }
+}
