96ca0d49c6b6ab4461fd168ce1a3dad2d11c9356
DropCodes <sagardb50@gmail.com>
2020-01-08 22:46:49 +0530

    added Queue class (int and char)

---
+++ src/DataStructure/Queue/Queue_chr.java
@@ -0,0 +1,41 @@
+package DataStructure.Queue;
+
+public class Queue_chr {
+    public char[] queue;
+    public int front;
+    public int rear;
+    public int size;
+
+    public Queue_chr(int size) {
+        this.size = size;
+        queue = new char[size];
+        front = 0;
+        rear = 0;
+    }
+
+    public void enqueue(char ele) {
+        if (rear == size) {
+            System.out.println("OverFlow");
+        } else {
+            queue[rear] = ele;
+            rear++;
+        }
+    }
+
+    public void dequeue() {
+        if (rear == front) {
+            System.out.println("UnderFlow");
+        } else {
+            queue[front] = 0;
+            front++;
+        }
+    }
+
+    public int front(){
+        return queue[front];
+    }
+
+    public boolean isEmpty(){
+        return (rear == front);
+    }
+}
