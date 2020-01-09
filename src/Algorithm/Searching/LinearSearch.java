1f24307c47b3c9af9cf9b4b56f9ff51cb985b74e
DropCodes <sagardb50@gmail.com>
2020-01-09 12:59:03 +0530

    Create LinearSearch.java

---
+++ src/Algorithm/Searching/LinearSearch.java
@@ -0,0 +1,19 @@
+package Algorithm.Searching;
+
+public class LinearSearch {
+    int[] array;
+    int ele;
+    public LinearSearch(int[] array,int ele){
+        this.array = array;
+        this.ele = ele;
+    }
+    public int search(){
+        for(int i = 0 ; i < array.length;i++){
+            if (array[i] == ele){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+}
